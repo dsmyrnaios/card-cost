2680e5ba9dbfe2778643cae3531cd8906887f804
dsmyrnaios <dsmyrnaios@gmail.com>
2019-12-18 15:26:45 +0200

    CardCost class

---
+++ Cardcost.Domain/Card.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Cardcost.Domain
+{
+    public class Card
+    {
+        [JsonProperty("card_number")]
+        public string CardNumber { get; set; }
+    }
+}
